476f12fcac14e9dc17395aa0f15f9051926d3f54
Mohammad Yazdani <yazdani.mohammad106@live.co.uk>
2021-04-21 15:01:13 -0400

    hh

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as child from 'child_process';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -44,12 +45,14 @@ export function activate(context: vscode.ExtensionContext) {
 					return;
 				}
 				let command = selection.description ? selection.description : "echo Nothing to do :/";
+				
 				const cp = require('child_process');
 				cp.exec(command, (err: string, stdout: string, stderr: string) => {
-					vscode.window.showInformationMessage('stdout: ' + stdout);
-					vscode.window.showInformationMessage('stderr: ' + stderr);
 					if (err) {
-						vscode.window.showInformationMessage('error: ' + err);
+						vscode.window.showInformationMessage('ERROR: ' + err);
+					} else {
+						vscode.window.showInformationMessage(stdout);
+						vscode.window.showInformationMessage(stderr);
 					}
 				});
 			});
